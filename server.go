d03140af0ec98b6287ced7472bbb8b364fae0b2b
nhatvu148 <nhatvu148@gmail.com>
2021-08-21 17:37:05 +0900

    middleware

--- server.go
+++ server.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	"github.com/nhatvu148/go-http/controller"
+	"github.com/nhatvu148/go-http/middleware"
 	"github.com/nhatvu148/go-http/service"
 )
 
@@ -11,8 +15,17 @@ var (
 	videoController controller.VideoController = controller.New(videoService)
 )
 
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
 func main() {
-	server := gin.Default()
+	setupLogOutput()
+
+	// server := gin.Default()
+	server := gin.New()
+	server.Use(gin.Recovery(), middleware.Logger(), middleware.BasicAuth())
 
 	server.GET("/test", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
